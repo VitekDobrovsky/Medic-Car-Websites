9c72cc0a9bab9c5466d21eb193ea7c9ee3fa5e3a
Vít Dobrovský <vit.dobrovsky@seznam.cz>
2024-11-11 18:17:44 +0100

    hamburger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,16 @@
+document.querySelectorAll(".faq__question").forEach((question) => {
+  question.addEventListener("click", () => {
+    const faqItem = question.parentElement;
+    faqItem.classList.toggle("open");
+  });
+});
 
-document.querySelectorAll('.faq__question').forEach(question => {
-    question.addEventListener('click', () => {
-        const faqItem = question.parentElement;
-        faqItem.classList.toggle('open');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navbarItems = document.getElementById("navbarItems");
+
+  hamburger.addEventListener("click", function () {
+    navbarItems.classList.toggle("active");
+    hamburger.classList.toggle("active"); // Toggle "X" icon
+  });
 });
